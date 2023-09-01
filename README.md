8185ca0f636c6d4ab05af7d1e05a66b2bb23b557
Eric Baptista <Eric Baptista>
2023-09-01 17:41:32 +0200

    add scripts

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sistock_api_tests
+
+`sistock_fail.py` and `sistock_success.py` require environment variable `SISTOCK_BASIC_AUTH` to be set. 
+
+`SISTOCK_BASIC_AUTH` should be `username:password` base64 encoded. 
---
+++ sistock_success.py
@@ -0,0 +1,19 @@
+import requests
+import os
+import sys
+
+ENDPOINT = 'https://sandbox.sistock.org/app/export/?op=delivery1'
+
+if not os.getenv('SISTOCK_BASIC_AUTH'):
+    print("ERROR: env var SISTOCK_BASIC_AUTH must be set")
+    sys.exit(1)
+
+auth = os.getenv('SISTOCK_BASIC_AUTH')
+
+header = {
+    'Authorization': f'Basic {auth}',
+    'User-Agent': ''
+}
+
+response = requests.get(ENDPOINT, headers=header)
+print(f"Response from {ENDPOINT}: {response.status_code}")
